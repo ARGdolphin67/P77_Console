cd0ed9a9c666de0c563434fb8f65d18c93f0ef61
ARGdolphin67 <109750556+ARGdolphin67@users.noreply.github.com>
2022-10-16 17:09:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name="Ranbir";
+console.log("Ranbir")
+
+var last_name="Kapoor";
+console.log("Kapoor")
+
+var a=10;
+console.log(a)
+
+var b=5;
+console.log(b)
+
+var add=a+b;
+console.log(add);
+
+f_name.concat(l_name);
